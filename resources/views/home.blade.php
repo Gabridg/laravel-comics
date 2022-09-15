bfa37ccf4c4eca02fd1553936e885663500b6fd2
Gabridg <gabri99.del@gmail.com>
2022-09-15 15:57:47 +0200

    creating header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,16 +11,29 @@
 </head>
 <body>
 <header>
+    <div id="notify">
+        <div class="container">    
+            <span>DC POWER VISA&copy</span>
+            <span>OTHER DC SITES</span>
+        </div>
+    </div>
     <div class="container">
         <div class="header-nav">
             <figure>
                 <img src="{{ asset('images/dc-logo.png')}} " alt="">
             </figure>
-            <div>
                 <ul>
-                    <li></li>
+                    <li><a href="{{ route('characters') }}">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
                 </ul>
-            </div>
         </div>
     </div>
 </header>
