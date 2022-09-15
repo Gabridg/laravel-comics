7cf1d8aa296099375e052cfa70b1a2a2242bb3e1
Gabridg <gabri99.del@gmail.com>
2022-09-15 15:09:59 +0200

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+    <figure>
+        <img src="{{ asset('images/logo.png')}} " alt="">
+    </figure>
+    <div>
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+    </div>
+</header>
