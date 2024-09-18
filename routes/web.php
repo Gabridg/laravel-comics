28308307a04f8a23746875ecb02ea5de25e1d655
Gabriele <102970931+Gabridg@users.noreply.github.com>
2024-09-18 15:07:18 +0200

    fixed web.php on home
    Restart to fix all my repositories

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    $comics = config('comics');
-    return view('home', ['comics' => $comics]);
-})->name('home');
 
 Route::get('/', function () {
     $comics = config('comics');
